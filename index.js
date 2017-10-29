eff40cfd4618d0d62a7220812201ae67eb90bc19
Eli Weinstock-Herman <tarwn@tiernok.com>
2017-10-29 12:10:54 -0400

    Tabs to spaces

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var express = require('express'),
-	exphbs   = require('express-handlebars'),
-	argv = require('yargs').argv;
+    exphbs = require('express-handlebars'),
+    argv = require('yargs').argv;
 
 // Express setup
 var app = express()
 
 var hbs = exphbs.create({
-	defaultLayout: 'main',
-	helpers: {
-		"each": function(context, options){
-			return context.reduce(function(ret, c){
-				return ret + options.fn(c);
-			}, '');
-			  
-			  var ret = "";
+    defaultLayout: 'main',
+    helpers: {
+        "each": function (context, options) {
+            return context.reduce(function (ret, c) {
+                return ret + options.fn(c);
+            }, '');
 
-			  for(var i=0, j=context.length; i<j; i++) {
-				ret = ret + options.fn(context[i]);
-			  }
+            var ret = "";
 
-			  return ret;		
-		}
-	}
+            for (var i = 0, j = context.length; i < j; i++) {
+                ret = ret + options.fn(context[i]);
+            }
+
+            return ret;
+        }
+    }
 });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -31,36 +31,36 @@ app.locals.title = 'Fictitious Store';
 
 // initialize data store
 var database = {
-	items: [
-		{ id: 1, name: 'Blue Thing', price: 5.01, stock: 99, imageUrl: '' }
-	],
-	orders: [],
-	users: [],
-	sessions: []
+    items: [
+        { id: 1, name: 'Blue Thing', price: 5.01, stock: 99, imageUrl: '' }
+    ],
+    orders: [],
+    users: [],
+    sessions: []
 }
 
 // Routes
 app.use(express.static('assets'))
 
-app.get('/test/allTheElements', function(req, res) {
+app.get('/test/allTheElements', function (req, res) {
     res.render('testAllTheElements');
 });
 
 
-app.get('/test/spa', function(req, res) {
+app.get('/test/spa', function (req, res) {
     res.render('testSPA');
 });
 
-app.get('/', function(req, res){
-	//TODO: add a random selection to make testing harder
-	// Situations:
-	//	Anonymous users
-	//	Potentially changing content
-	var topItems = database.items.slice(0, 5);
-	res.render('homepage', { topItems: topItems });	
+app.get('/', function (req, res) {
+    //TODO: add a random selection to make testing harder
+    // Situations:
+    //	Anonymous users
+    //	Potentially changing content
+    var topItems = database.items.slice(0, 5);
+    res.render('homepage', { topItems: topItems });
 });
 
 var port = argv.port;
 app.listen(port, function () {
-	console.log('Fictitious Company Inc App: Running on port ' + port);
+    console.log('Fictitious Company Inc App: Running on port ' + port);
 })
