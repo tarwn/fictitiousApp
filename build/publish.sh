1717eb78ab9f7a45512849094e3e42fe04170df1
Eli Weinstock-Herman <tarwn@tiernok.com>
2017-10-29 14:29:01 -0400

    Stamp version number in file

--- build/publish.sh
+++ build/publish.sh
@@ -4,5 +4,6 @@ git config --global user.name "$USER_NAME"
 
 git checkout -b "$1"
 git merge master
+echo '{ "version": "$CIRCLE_BUILD_NUM" }' > app/version.json
 git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
 git push --force --set-upstream origin $1
