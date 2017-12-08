51255ef08215c5eda2f57ea5cfe728c4cfcb7aa0
Eli Weinstock-Herman <tarwn@tiernok.com>
2017-12-07 20:48:04 -0500

    npm install prior to lr

--- build/travis-launchready.sh
+++ build/travis-launchready.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd $TRAVIS_BUILD_DIR/build/launchready
+npm install
 node build.js $LR_APPLICATIONID --appId=$LR_APPID --apiKey=$LR_APIKEY --name="TravisCI-$TRAVIS_BUILD_NUMBER" --baseURL=https://$TRAVIS_APP_NAME_STAGING.azurewebsites.net --savePath=results.xml
